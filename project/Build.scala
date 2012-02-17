e4babbaa9ca497e251ac30eda41abc5de3e09621
Henrik Engstrom <henrik.engstrom@gmail.com>
2012-02-17 18:49:49 +0100

    Initial setup

---
+++ project/Build.scala
@@ -0,0 +1,10 @@
+import sbt._
+import Keys._
+
+object Build extends Build {
+  lazy val root = Project(id = "playminisample", 
+  	base = file("."), settings = Project.defaultSettings).settings(
+    resolvers += "Typesafe Snapshot Repo" at "http://repo.typesafe.com/typesafe/snapshots/",
+    libraryDependencies += "com.typesafe" %% "play-mini" % "2.0-RC3-SNAPSHOT",
+    mainClass in (Compile, run) := Some("play.core.server.NettyServer"))
+}
