8a895b64a44791edb3e7fbcd2031be27af946690
Henrik Engstrom <henrik.engstrom@gmail.com>
2012-02-20 09:42:00 +0100

    Added Typesafe Repo to build file.

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,7 @@ import Keys._
 object Build extends Build {
   lazy val root = Project(id = "playminisample", 
   	base = file("."), settings = Project.defaultSettings).settings(
+    resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
     resolvers += "Typesafe Snapshot Repo" at "http://repo.typesafe.com/typesafe/snapshots/",
     libraryDependencies += "com.typesafe" %% "play-mini" % "2.0-RC3-SNAPSHOT",
     mainClass in (Compile, run) := Some("play.core.server.NettyServer"))
